31fbde04b7949958f4cdc26bf41a74dd0fdcb4f5
VictorAkiyama <victorakio11@gmail.com>
2025-08-22 11:01:04 -0300

    Create VILAO.java

---
+++ JAVA/FPOO-dinamica-28-08-2025/VILAO.java
@@ -0,0 +1,16 @@
+package dinamica;
+
+public class VILAO {
+	
+	String nome;
+	private String habilidade;
+	
+	//DANDO VALOR PARA ATRIBUTO DE OBJETO COM VALOR DEFINIDO DE OBJETO DA CLASSE
+	public void setHabilidade(String habilidadeDocumentada) {
+		this.habilidade = habilidadeDocumentada;
+	}
+	//PEGANDO VALOR E RETORNANDO
+	public String getHabilidade() {
+		return habilidade;
+	}
+}
