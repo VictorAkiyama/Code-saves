9150861118390f7c575c068759c9c2092a3c2613
VictorAkiyama <victorakio11@gmail.com>
2025-09-25 09:12:32 -0300

    Create POUPANCA.java

---
+++ JAVA/VictorAkiyama-FPOO-Atividade2-ClassesAbstrata/POUPANCA.java
@@ -0,0 +1,18 @@
+package Contapackage;
+
+public class POUPANCA extends CONTA {
+	
+	@Override
+	public double Transferir() {
+		return this.dinheiro = dinheiro;
+	}
+	public double Depositar() {
+		return this.dinheiro + dinheiro;
+	}
+	public double Sacar() {
+		return this.dinheiro - dinheiro;
+	}
+	public double Saldo() {
+		return this.dinheiro = dinheiro * 1.005;
+	}
+}
