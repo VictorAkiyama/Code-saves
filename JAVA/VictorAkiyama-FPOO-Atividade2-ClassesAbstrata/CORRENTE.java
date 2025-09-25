c3a88ec77636a9e8eb3f8ddbaf72aae064d92cf8
VictorAkiyama <victorakio11@gmail.com>
2025-09-25 09:12:59 -0300

    Create CORRENTE.java

---
+++ JAVA/VictorAkiyama-FPOO-Atividade2-ClassesAbstrata/CORRENTE.java
@@ -0,0 +1,18 @@
+package Contapackage;
+
+public class CORRENTE extends CONTA{
+	
+	@Override
+	public double Transferir() {
+		return this.dinheiro = dinheiro;
+	}
+	public double Depositar() {
+		return this.dinheiro + dinheiro;
+	}
+	public double Sacar() {
+		return this.dinheiro - dinheiro;
+	}
+	public double Saldo() {
+		return this.dinheiro;
+	}
+}
