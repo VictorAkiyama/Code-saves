d295d3bf5bb7d5d2dcf0c06a82f83a4bdb24873f
VictorAkiyama <victorakio11@gmail.com>
2025-09-26 19:56:26 -0300

    Create main.java, Creations and Simple-Example-Encapsulation-Gets&Sets folders

---
+++ JAVA/Creations/Simple-Example-Encapsulation-Gets&Sets/main.java
@@ -0,0 +1,11 @@
+public class Main
+{
+	public static void main(String[] args) {
+	    
+	    CLIENTE cliente1 = new CLIENTE();
+	    
+	    cliente1.setnome("Joe");
+	    
+		System.out.println(cliente1.getnome());
+	}
+}
