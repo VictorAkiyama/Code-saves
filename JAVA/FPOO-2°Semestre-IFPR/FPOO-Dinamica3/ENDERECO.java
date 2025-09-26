5cd2e7e992a69008d989dfd796d4b7160e10cfa7
VictorAkiyama <victorakio11@gmail.com>
2025-09-26 09:15:52 -0300

    Create ENDERECO.java

---
+++ JAVA/FPOO-2°Semestre-IFPR/FPOO-Dinamica3/ENDERECO.java
@@ -0,0 +1,41 @@
+public class ENDERECO {
+
+	private String rua;
+	private String bairro;
+	private String cidade;
+	private String cep;
+	
+	//Gets
+		public String getrua() {
+			return this.rua;
+		}
+		
+		public String getbairro() {
+			return this.bairro;
+		}
+		
+		public String getcidade() {
+			return this.cidade;
+		}
+		
+		public String getcep() {
+			return this.cep;
+		}
+	
+	//Sets
+	public String setrua() {
+		return this.rua = rua;
+	}
+	
+	public String setbairro() {
+		return this.bairro = bairro;
+	}
+	
+	public String setcidade() {
+		return this.cidade = cidade;
+	}
+	
+	public String setcep() {
+		return this.cep = cep;
+	}
+}
