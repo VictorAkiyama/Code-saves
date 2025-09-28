46dc82d431ac8dcc9041ffbd61fae1d6e49a363e
VictorAkiyama <victorakio11@gmail.com>
2025-09-28 15:22:13 -0300

    Update and rename CONTA.java to CONTA_CLIENTE.java

---
+++ JAVA/VictorAkiyama-FPOO-Atividade2-ClassesAbstrata/CONTA_CLIENTE.java
@@ -0,0 +1,35 @@
+package Contapackage;
+
+public abstract class CONTA_CLIENTE {
+	
+	protected String nome;
+	protected String cpf;
+	private double dinheiro;
+	protected double renda;
+	
+	//Construtor vazio
+	public CONTA_CLIENTE() {}
+	
+	//Construtor cheio
+    public CONTA_CLIENTE(String nome, String cpf, double dinheiro, double renda) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.dinheiro = dinheiro;
+        this.renda = renda;
+    }
+	
+	//Métodos
+	public abstract void Transferir(double qtd_transferir, CONTA_CLIENTE conta_destino);
+	public abstract double Depositar(double qtd_depositar);
+	public abstract double Sacar(double qtd_sacar);
+	public abstract double Saldo();
+	public abstract double Renda();
+	
+	//Gets e Sets
+	public double getDinheiro() {
+		return this.dinheiro;
+	}
+	public void setDinheiro(double dinheiro) {
+		this.dinheiro = dinheiro;
+	}
+}
