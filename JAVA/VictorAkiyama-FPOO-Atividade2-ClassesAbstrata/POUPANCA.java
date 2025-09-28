f78cb6f6c376f570c7ab683917b4d3e73362a7dd
VictorAkiyama <victorakio11@gmail.com>
2025-09-28 15:21:31 -0300

    Update POUPANCA.java

--- JAVA/VictorAkiyama-FPOO-Atividade2-ClassesAbstrata/POUPANCA.java
+++ JAVA/VictorAkiyama-FPOO-Atividade2-ClassesAbstrata/POUPANCA.java
@@ -1,18 +1,30 @@
 package Contapackage;
 
-public class POUPANCA extends CONTA {
-	
+public class POUPANCA extends CONTA_CLIENTE {
+    
+    //Construtor vazio
+    public POUPANCA() {}
+    
+    //Construtor cheio
+    public POUPANCA(String nome, String cpf, double dinheiro, double renda) {
+        super(nome, cpf, dinheiro, renda);
+    }
+    
 	@Override
-	public double Transferir() {
-		return this.dinheiro = dinheiro;
+	public void Transferir(double qtd_transferir, CONTA_CLIENTE conta_destino) {
+		this.setDinheiro(this.getDinheiro() - qtd_transferir);
+		conta_destino.setDinheiro(conta_destino.getDinheiro() + qtd_transferir);
 	}
-	public double Depositar() {
-		return this.dinheiro + dinheiro;
+	public double Depositar(double qtd_depositar) {
+		return this.getDinheiro() + qtd_depositar;
 	}
-	public double Sacar() {
-		return this.dinheiro - dinheiro;
+	public double Sacar(double qtd_sacar) {
+		return this.getDinheiro() - qtd_sacar;
 	}
 	public double Saldo() {
-		return this.dinheiro = dinheiro * 1.005;
+		return this.getDinheiro();
+	}
+	public double Renda() {
+	    return this.getDinheiro() * renda;
 	}
 }
