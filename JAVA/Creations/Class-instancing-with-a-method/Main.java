ec1f82ed16b8f5a808b8b894401784c4c0d92a31
VictorAkiyama <victorakio11@gmail.com>
2025-10-17 10:55:39 -0300

    Update Main.java
    Deleting a useless comment, that used to hide a placeholder class instancing that wouldn´t be viable to implement

--- JAVA/Creations/Class-instancing-with-a-method/Main.java
+++ JAVA/Creations/Class-instancing-with-a-method/Main.java
@@ -2,7 +2,6 @@ public class Main
 {
 	public static void main(String[] args) {
 		Sistema sistema = new Sistema();
-		//Consulta consulta = new Consulta("", "");
 		Consulta consulta = sistema.CriarConsulta("Dr.José", "Felipe");
 		System.out.println("Dentista: "+ consulta.dentista + "; Paciente: " + consulta.paciente + ".");
 	}
