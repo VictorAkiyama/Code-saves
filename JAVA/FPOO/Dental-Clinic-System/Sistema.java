4821c4a4604e1a392a2ce56995c28c97ed84decc
VictorAkiyama <victorakio11@gmail.com>
2025-11-22 19:36:59 -0300

    Update Sistema.java

--- JAVA/FPOO/Dental-Clinic-System/Sistema.java
+++ JAVA/FPOO/Dental-Clinic-System/Sistema.java
@@ -6,16 +6,16 @@ public class Sistema {
 	public Sistema() {}
 	
 	//métodos
-	public static Consulta CriarConsulta(String dentista, String paciente, int horario) {
-        Consulta novaConsulta = new Consulta(dentista, paciente, horario);
+	public Consulta CriarConsulta(Dentista dentista, Paciente paciente, int horario, String detalhes, Acompanhamento acompanhamentos[]) {
+        Consulta novaConsulta = new Consulta(dentista, paciente, horario, detalhes, acompanhamentos);
         return novaConsulta;
     }
-	public static Consulta CriarConsulta() {
+	public Consulta CriarConsulta() {
 		Consulta novaConsulta = new Consulta();
 		return novaConsulta;
 	}
 	
-	public Consulta interacaoConsulta(int opcao, Consulta consulta, Sistema sistema) {
+	public Consulta interacaoConsulta(int opcao, Consulta consulta, Sistema sistema, Consulta consultaLista[]) {
 		//opções da tela principal
 		System.out.println("================================================");
 		System.out.println("           Opção 1: Marcar Consulta             ");
@@ -28,16 +28,18 @@ public class Sistema {
 				System.out.println("Opção para marcar selecionada");
 				return consulta.marcar(consulta, sistema);
 			}
-			/*
 			else if (opcao == 2) {
 				consulta.remarcar();
 			}
 			else if (opcao == 3) {
-				consulta.cancelar();
+				consulta.cancelar(consulta, sistema, consultaLista);
 			}
 			else if (opcao == 4) {
-				consulta.acompanhamento();
-			}*/
+				System.out.println("Opção para marcar acompanhamento selecionada\nQual das consultas deseja marcar um acompanhamento:");
+				sistema.mostrarListadeConsultas(consultaLista);
+				//simula o usuário selecionando uma das consultas
+				consulta.acompanhamento(consultaLista, 0);
+			}
 			else {
 				System.out.println(opcao + " Não é uma opção válida, tente novamente");
 				return new Consulta();
@@ -46,12 +48,28 @@ public class Sistema {
 		return new Consulta();
 	}
 	
-	public void colocarNovaConsultaEmLista(Consulta consultaInteracao, Consulta consultaLista[]) {
+	public Consulta colocarNovaConsultaEmLista(Consulta consultaInteracao, Consulta consultaLista[]) {
 		int i;
 		for (i = 0; i < 10; i++) {
 			if (consultaLista[i] == null) {
-				consultaLista[i]= consultaInteracao;
-				return;
+				consultaLista[i] = consultaInteracao;
+				//sai do loop
+				return new Consulta();
+			}
+		}
+		//limpa consultaInteracao
+		return new Consulta();
+	}
+	
+	public void mostrarListadeConsultas(Consulta consultaLista[]) {
+		int i;
+		System.out.println("\nLista de consultas:");
+		for (i = 0; i < 10; i++) {
+			if (consultaLista[i] == null) {
+				System.out.println(i + " Nenhuma consulta marcada aqui.");
+			}
+			else {
+				System.out.println(i + " " + consultaLista[i].dentista.nome + ", " + consultaLista[i].paciente.nome + ", " + consultaLista[i].horario + ":00, " + consultaLista[i].detalhes + ";");
 			}
 		}
 	}
