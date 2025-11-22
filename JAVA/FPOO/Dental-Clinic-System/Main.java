24e9e13927d77c3cad208e3ef55a7cbbac0132a2
VictorAkiyama <victorakio11@gmail.com>
2025-11-22 19:36:17 -0300

    Update Main.java

--- JAVA/FPOO/Dental-Clinic-System/Main.java
+++ JAVA/FPOO/Dental-Clinic-System/Main.java
@@ -8,12 +8,24 @@ public class Main {
 		Consulta consultaLista[] = new Consulta[10];
 		
 		//simulação de o usuário digitando para marcar consulta, opção válida
-		Consulta consultaInteracao = sistema.interacaoConsulta(1, consulta, sistema);
+		Consulta consultaInteracao = sistema.interacaoConsulta(1, consulta, sistema, consultaLista);
 		
 		//nova consulta feita pelo usuario colocado em uma lista
-		sistema.colocarNovaConsultaEmLista(consultaInteracao, consultaLista);
+		consultaInteracao = sistema.colocarNovaConsultaEmLista(consultaInteracao, consultaLista);
 		
-		//temp para ver se foi marcado na lista
-		System.out.println(consultaLista[0].dentista + " " + consultaLista[0].paciente + " " + consultaLista[0].horario);
+		//mostra lista de consultas
+		sistema.mostrarListadeConsultas(consultaLista);
+		
+		/*	TESTES	*/
+		
+		//simulação de cancelamento da consulta
+		//consultaInteracao = sistema.interacaoConsulta(3, consulta, sistema, consultaLista);
+		
+		//mostra lista de consultas
+		//sistema.mostrarListadeConsultas(consultaLista);
+		
+		//simulação de criar acompanhamento
+		consultaInteracao = sistema.interacaoConsulta(4, consulta, sistema, consultaLista);
+		
 	}
 }
