9b74c3f3932209b5ec3ecd0f90d62a34cca5e0a6
VictorAkiyama <victorakio11@gmail.com>
2025-11-24 16:51:34 -0300

    Create Agenda.java

---
+++ JAVA/FPOO/Dental-Clinic-System/Agenda.java
@@ -0,0 +1,33 @@
+package SistemaClinicaOdontologicaPackage;
+
+public class Agenda {
+
+	public boolean hora8 = false;
+	public boolean hora9 = false;
+	public boolean hora10 = false;
+	public boolean hora11 = false;
+	public boolean hora12 = false;
+	public boolean hora13 = false;
+	public boolean hora14 = false;
+	public boolean hora15 = false;
+	public boolean hora16 = false;
+	public boolean hora17 = false;
+	public boolean hora18 = false;
+	
+	//construtor
+	public Agenda() {}
+	
+	public Agenda(boolean hora8, boolean hora9, boolean hora10, boolean hora11, boolean hora12, boolean hora13, boolean hora14, boolean hora15, boolean hora16, boolean hora17, boolean hora18) {
+		this.hora8 = hora8;
+		this.hora9 = hora9;
+		this.hora10 = hora10;
+		this.hora11 = hora11;
+		this.hora12 = hora12;
+		this.hora13 = hora13;
+		this.hora14 = hora14;
+		this.hora15 = hora15;
+		this.hora16 = hora16;
+		this.hora17 = hora17;
+		this.hora18 = hora18;
+	}
+}
