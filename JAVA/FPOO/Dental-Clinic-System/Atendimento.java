c81222f12ccf4fa44d7579c241167fafc276fb53
VictorAkiyama <victorakio11@gmail.com>
2025-11-24 16:47:08 -0300

    Update Atendimento.java

--- JAVA/FPOO/Dental-Clinic-System/Atendimento.java
+++ JAVA/FPOO/Dental-Clinic-System/Atendimento.java
@@ -3,6 +3,6 @@ package SistemaClinicaOdontologicaPackage;
 public interface Atendimento {
 	
 	public abstract Consulta marcar(Consulta consulta, Sistema sistema);
-	public abstract void remarcar();
+	public abstract Consulta remarcar(Consulta consulta, Sistema sistema, Consulta consultaInteracao, Agenda agenda);
 	public abstract Consulta cancelar(Consulta consulta, Sistema sistema, Consulta consultaLista[]);
 }
