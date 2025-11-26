e1c8773157a2c213abcaabd332f5549973c0acbe
VictorAkiyama <victorakio11@gmail.com>
2025-11-26 09:50:12 -0300

    Update Acompanhamento.java

--- JAVA/FPOO/Dental-Clinic-System/Acompanhamento.java
+++ JAVA/FPOO/Dental-Clinic-System/Acompanhamento.java
@@ -2,8 +2,8 @@ package SistemaClinicaOdontologicaPackage;
 
 public class Acompanhamento {
 
-	public Consulta consulta;
-	public int horario;
+	private Consulta consulta;
+	private int horario;
 	
 	//construtor
 	public Acompanhamento() {};
@@ -17,7 +17,7 @@ public class Acompanhamento {
 	public Acompanhamento marcar(Consulta consulta, int horario) {
 		Acompanhamento acompanhamento = new Acompanhamento(consulta, horario);
 		if (consulta != null) {
-			System.out.println("Acompanhamento marcado com Dr." + consulta.dentista.nome + ", para o paciente: " + consulta.paciente.nome + " às " + horario + ":00.");
+			System.out.println("Acompanhamento marcado com Dr." + consulta.getdentista().getnome() + ", para o paciente: " + consulta.getpaciente().getnome() + " às " + horario + ":00.");
 		}
 		return acompanhamento;
 	}
@@ -25,7 +25,7 @@ public class Acompanhamento {
 	public Acompanhamento remarcar(Consulta consulta, int horario) {
 		Acompanhamento acompanhamento = new Acompanhamento(consulta, horario);
 		if (consulta != null) {
-			System.out.println("Acompanhamento remarcado com Dr." + consulta.dentista.nome + ", para o paciente: " + consulta.paciente.nome + " às " + horario + ":00.");
+			System.out.println("Acompanhamento remarcado com Dr." + consulta.getdentista().getnome() + ", para o paciente: " + consulta.getpaciente().getnome() + " às " + horario + ":00.");
 		}
 		return acompanhamento;
 	}
@@ -33,7 +33,7 @@ public class Acompanhamento {
 	public Acompanhamento cancelar(Consulta consulta) {
 		Acompanhamento acompanhamento = new Acompanhamento(consulta, horario);
 		if (consulta != null) {
-			System.out.println("Acompanhamento de consulta: " + consulta.id_consulta + " cancelado.");
+			System.out.println("Acompanhamento de consulta: " + consulta.getid_consulta() + " cancelado.");
 		}
 		return acompanhamento;
 	}
@@ -42,5 +42,20 @@ public class Acompanhamento {
 	@Override
 	public String toString() {
 		return "Acompanhamento da consulta marcado às: " + horario + ":00";
+	}
+	
+	//gets e sets
+	public Consulta getconsulta() {
+		return consulta;
+	}
+	public void setconsulta(Consulta consulta) {
+		this.consulta = consulta;
+	}
+	
+	public int gethorario() {
+		return horario;
+	}
+	public void sethorario(int horario) {
+		this.horario = horario;
 	}
  }
