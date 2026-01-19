c9f9a17b284b26b638b28a43952815fa3cd2d965
VictorAkiyama <victorakio11@gmail.com>
2026-01-19 18:13:06 -0300

    Update classifica-triangulo.c
    solving the multiline syntax error ''' and changing to /* */

--- C/Creations/classifica-triangulo.c
+++ C/Creations/classifica-triangulo.c
@@ -1,10 +1,10 @@
 /*Classificador de triângulo*/
-''' Dados três valores A, B e C, verificar se eles podem ser os comprimentos dos lados de
+/* Dados três valores A, B e C, verificar se eles podem ser os comprimentos dos lados de
 um triângulo. Caso for, verificar se é um triângulo equilátero, isósceles ou escaleno. A
 condição básica para ser um triângulo é que qualquer lado do triângulo não pode ser
 maior ou igual do que a soma dos outros dois lados. Entrada: três valores que
 representam lados do suposto triângulo (A, B e C). Saída: mensagem dizendo qual tipo
-de triângulo se encaixa ou dizer que não é um triângulo se for o caso. '''
+de triângulo se encaixa ou dizer que não é um triângulo se for o caso. */
 
 #include <stdio.h>
 int main() {
