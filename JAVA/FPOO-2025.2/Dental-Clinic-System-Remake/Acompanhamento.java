6beb66b4ec06849011c2ae2b02aa46f5178647ed
VictorAkiyama <victorakio11@gmail.com>
2026-02-16 16:34:31 -0300

    Update Acompanhamento.java (16/02/2026)

--- JAVA/FPOO-2025.2/Dental-Clinic-System-Remake/Acompanhamento.java
+++ JAVA/FPOO-2025.2/Dental-Clinic-System-Remake/Acompanhamento.java
@@ -3,37 +3,41 @@ package SistemaClinicaOdontologicaPackage;
 public class Acompanhamento {
 
 	private Consulta consulta;
+	private String nomeDentista;
 	private int horario;
 	
 	//construtor
 	public Acompanhamento() {};
 	
-	public Acompanhamento(Consulta consulta, int horario) {
+	public Acompanhamento(Consulta consulta, String nomeDentista, int horario) {
 		this.consulta = consulta;
+		this.nomeDentista = nomeDentista;
 		this.horario = horario;
 	}
 	
 	//métodos
-	public Acompanhamento marcar(Consulta consulta, int horario) {
-		Acompanhamento acompanhamento = new Acompanhamento(consulta, horario);
+	public Acompanhamento marcar(Consulta consulta, String nomeDentista, int horario) {
+		Acompanhamento acompanhamento = new Acompanhamento(consulta, nomeDentista, horario);
 		if (consulta != null) {
-			System.out.println("Acompanhamento marcado com Dr." + consulta.getdentista().getnome() + ", para o paciente: " + consulta.getpaciente().getnome() + " às " + horario + ":00.");
+			System.out.println("Acompanhamento marcado com Dr." + acompanhamento.getnomeDentista() + ", para o paciente: " + consulta.getpaciente().getnome() + " às " + acompanhamento.gethorario() + ":00.");
 		}
 		return acompanhamento;
 	}
 	
-	public Acompanhamento remarcar(Consulta consulta, int horario) {
-		Acompanhamento acompanhamento = new Acompanhamento(consulta, horario);
+	public Acompanhamento remarcar(Consulta consulta, String nomeDentista, int horario) {
+		Acompanhamento acompanhamento = new Acompanhamento(consulta, nomeDentista, horario);
 		if (consulta != null) {
-			System.out.println("Acompanhamento remarcado com Dr." + consulta.getdentista().getnome() + ", para o paciente: " + consulta.getpaciente().getnome() + " às " + horario + ":00.");
+			System.out.println("Acompanhamento remarcado com Dr." + acompanhamento.getnomeDentista() + ", para o paciente: " + consulta.getpaciente().getnome() + " às " + acompanhamento.gethorario() + ":00.");
 		}
 		return acompanhamento;
 	}
 	
 	public Acompanhamento cancelar(Consulta consulta) {
-		Acompanhamento acompanhamento = new Acompanhamento(consulta, horario);
+		String nomeDentista = null;
+		int horario = 0;
+		Acompanhamento acompanhamento = new Acompanhamento(consulta, nomeDentista, horario);
 		if (consulta != null) {
-			System.out.println("Acompanhamento de consulta: " + consulta.getid_consulta() + " cancelado.");
+			System.out.println("Acompanhamento de consulta: " + consulta.getid_consulta() + "com o Dr." + acompanhamento.getnomeDentista() + " marcado às " + acompanhamento.gethorario() + ":00, foi cancelado.");
 		}
 		return acompanhamento;
 	}
@@ -41,7 +45,7 @@ public class Acompanhamento {
 	//para mostrar acompanhamento ao chama-lo
 	@Override
 	public String toString() {
-		return "Acompanhamento da consulta marcado às: " + horario + ":00";
+		return "Acompanhamento da consulta marcado às: " + horario + ":00, com o Dr: " + nomeDentista;
 	}
 	
 	//gets e sets
@@ -50,6 +54,13 @@ public class Acompanhamento {
 	}
 	public void setconsulta(Consulta consulta) {
 		this.consulta = consulta;
+	}
+	
+	public String getnomeDentista() {
+		return nomeDentista;
+	}
+	public void setnomeDentista(String nomeDentista) {
+		this.nomeDentista = nomeDentista;
 	}
 	
 	public int gethorario() {
